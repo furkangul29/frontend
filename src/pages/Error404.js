665b8f9c3d7aa541fe16eb164b55d41474503f98
furkangul29 <gulfurkan@outlook.com.tr>
2024-06-03 15:16:39 +0300

    login ve error sayfası ayarlandı yonlendirmeleri verildi.

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,24 +1,41 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "../errorPage.css";
 
 function Error404() {
+  const navigate = useNavigate();
+
+  const goToHomePage = () => {
+    navigate("/");
+  };
+
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="xs-12 md-6 mx-auto">
-            <div id="countUp">
-              <div class="number" data-count="404">
-                0
-              </div>
-              <div class="text">Page not found</div>
-              <div class="text">This may not mean anything.</div>
-              <div class="text">
-                I'm probably working on something that has blown up.
+      <section className="page_404">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 ">
+              <div className="col-sm-10 col-sm-offset-1  text-center">
+                <div className="four_zero_four_bg">
+                  <h1 className="text-center ">404</h1>
+                </div>
+                <div className="contant_box_404">
+                  <h1 className="h2">Görünüşe Göre Kaybolmuş Gibisin</h1>
+                  <p>Aradığın sayfa mevcut Değil!</p>
+                  <Button
+                    onClick={goToHomePage}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Giriş Sayfasına Git
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
